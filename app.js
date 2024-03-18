dc7651e981fe6df8d9fa731d49e582c5ce10f9dd
muhammadyasa <muhammadyasa307@gmail.com>
2024-03-18 23:17:01 +0500

    add

---
+++ app.js
@@ -0,0 +1,60 @@
+// q1
+// let myNames = [];
+
+// q2
+// let myNames = new Array();
+
+// q3
+// let stringsArray = ["onion", "tomato", "carrot"];
+
+// q4
+//let numbersArray = [2, 4, 6, 8, 10];
+
+// q5
+// let booleanArray = [true, false, true];
+
+// q6
+//let mixedArray = ["apple", 3, true, "banana", false, 7];
+
+// q7
+// let educationQualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+// // Displaying qualifications in the browser
+// document.write("<h3>Qualifications:</h3>");
+// document.write("<ol>");
+// for (let i = 0; i < educationQualifications.length; i++) {
+//     document.write("<li>" + educationQualifications[i] + "</li>");
+// }
+// document.write("</ol>");
+
+// q8
+// Array to store student names
+// let studentNames = ["Michael", "John", "Tony"];
+
+// Array to store scores of students
+// let scores = [320, 230, 420];
+
+// Calculate percentages
+// let totalMarks = 500;
+// let percentages = [];
+// for (let i = 0; i < scores.length; i++) {
+//     let percentage = (scores[i] / totalMarks) * 100;
+//     percentages.push(percentage.toFixed(2));
+// }
+
+// Displaying scores and percentages
+// document.write("<h3>Student Scores and Percentages:</h3>");
+// document.write("<table border='1'>");
+// document.write("<tr><th>Student Name</th><th>Score</th><th>Percentage</th></tr>");
+// for (let i = 0; i < studentNames.length; i++) {
+//     document.write("<tr>");
+//     document.write("<td>" + studentNames[i] + "</td>");
+//     document.write("<td>" + scores[i] + "</td>");
+//     document.write("<td>" + percentages[i] + "%</td>");
+//     document.write("</tr>");
+// }
+// document.write("</table>");
+
+
+ 
+
